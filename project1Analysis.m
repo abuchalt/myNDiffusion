20cbb475aae04b8deb211d909f73ee0a532c599a
Adam Buchalter <abuchalt@vols.utk.edu>
2025-09-09 18:07:29 -0400

    added analytical flux comparison

--- project1Analysis.m
+++ project1Analysis.m
@@ -143,3 +143,124 @@ title('Centerline Flux Profile for Different Grid Spacings','interpreter','latex
 xlim([0 A])
 filename = 'lineProfileFlux.jpg';
 saveas(figure(2),fullfile(mydir,filename));
+
+%% Plot Centerline Profiles
+% ------------------------------------------------------------------------------
+
+% Input parameters
+Sigma_tr = 3.62E-2; % Macroscopic Transport Cross-Section [cm^-1]
+Sigma_a = 0.1532; % Macroscopic Absorption Cross-Section [cm^-1]
+nuSigma_f = 0.1570; % Product of Macroscopic Fission Cross-Section and Neutrons per Fission [cm^-1]
+% Calculated Constant Parameters
+D = 1.0/(3.0*(Sigma_a+Sigma_tr)); % Diffusion Coefficient [cm]
+L = sqrt(D/Sigma_a); % Characteristic Diffusion Length [cm]
+
+% Analytical Critical Dimension [cm]
+critDim = pi * sqrt(2.0*D/(nuSigma_f-Sigma_a));
+
+% Define physical domain
+A = critDim; % x-Length [cm]
+B = critDim; % y-Width [cm]
+
+% Init
+errList = zeros(meshes, 1);
+
+% Gather and Plot Errors in Solution Matrices
+figure(3);
+for i = 1:meshes
+    N = meshSizes(i);
+    subfolder=string(N)+'x'+string(N);
+    myphi = matfile(fullfile(mydir,subfolder,'phiPlot.mat'));
+    phi = reshape(myphi.phiref2Plot, N, N);
+    midline = (N+1)/2;
+
+    % Define x and y values in complete spatial domain
+    Deltax = A/(N-1);
+    fullx = zeros(N,1);
+    for j = 1:N
+        fullx(j) = Deltax*(j-1);
+    end
+
+    myErrProfile = phi(midline,:)-transpose(analytical(fullx, B/2, A, B));
+    
+    % errList(i) = sum(myErrProfile);
+
+    plot(fullx, myErrProfile, 'DisplayName', subfolder);
+    hold on;
+end
+hold off;
+legend;
+ylabel('Difference in Normalized Flux','interpreter','latex');
+xlabel('x-Axis Distance [cm]','interpreter','latex');
+title('Error in Centerline Flux Profile for Different Grid Spacings','interpreter','latex');
+xlim([0 A])
+filename = 'globalErrorFlux.jpg';
+saveas(figure(3),fullfile(mydir,filename));
+
+%% Calculate Global Errors
+% ------------------------------------------------------------------------------
+
+for i = 1:meshes
+    N = meshSizes(i);
+    subfolder=string(N)+'x'+string(N);
+    myphi = matfile(fullfile(mydir,subfolder,'phiPlot.mat'));
+    phi = reshape(myphi.phiref2Plot, N, N);
+    midline = (N+1)/2;
+
+    analyticalSurface = zeros(N,N);
+
+    % Define x and y values in complete spatial domain
+    Deltax = A/(N-1);
+    Deltay = B/(N-1);
+    fullx = zeros(N,1);
+    for j = 1:N
+        for k = 1:N
+            x = Deltax*(j-1);
+            y = Deltay*(k-1);
+            analyticalSurface(j,k) = analytical(x, y, A, B);
+        end
+    end
+
+    myErrSurface = phi-analyticalSurface;
+
+    errList(i) = sum(myErrSurface,"all")/(N^2);
+end
+
+%% Graphically Estimate Order of Convergence by Global Error
+% ------------------------------------------------------------------------------
+
+r = 2;
+p2 = log(norm(errList(1,1)-errList(2,1))/norm(errList(2,1)-errList(3,1)))/log(r);
+fprintf('Order of Grid Convergence by Error: %g\n', p2);
+
+% Demonstrate Order of Convergence Graphically
+h = 1./meshSizes;
+% Transform Into Expected Linear Behavior Space
+h2 = h.^2;
+% Line of Fit
+coefficients = polyfit(h2, errList, 1);
+xFit = linspace(min(h2), max(h2), 3);
+yFit = polyval(coefficients , xFit);
+% Inspect Closeness
+figure(4);
+plot(h2, errList, '.', 'MarkerSize', 15); % Plot Real Data
+hold on;
+plot(xFit, yFit, 'k--'); % Plot Fit
+hold on;
+yl.LabelHorizontalAlignment = 'center';
+yl.Color = [.90 0 0];
+hold off;
+ylabel('Global Error in Flux Field','interpreter','latex');
+xlabel('Square of Relative Grid Spacing $h^2$','interpreter','latex');
+title('Visualize Convergence by Global Relative Error','interpreter','latex');
+set ( gca, 'XDir', 'reverse' )
+filename = 'GridConvergence_GlobalError.jpg';
+saveas(figure(4),fullfile(mydir,filename));
+
+%% Functions
+% ------------------------------------------------------------------------------
+
+% Analytical Flux Function
+function phi = analytical(x, y, A, B)
+    phi = sin(pi.*x/A)*sin(pi.*y/A)/(4*A*B/pi^2);
+end
